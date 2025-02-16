df5b09bdef19fcbf0c43f48417abbdf10b490524
shuvad23 <shuvad29@gmail.com>
2025-02-17 03:25:18 +0600

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let temporaryContent = document.getElementById('temporaryContent');
 
 // load layout file
 const loadTemplate = () => {
-    fetch('/template.html')
+    fetch('./template.html')
     .then(response => response.text())
     .then(html => {
         app.innerHTML = html;
