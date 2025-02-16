ba50af16c4ce30f72688b2c1fed3fcfc8d6ada6e
shuvad23 <shuvad29@gmail.com>
2025-02-17 03:46:12 +0600

    updated detail.js file

--- detail.js
+++ detail.js
@@ -1,23 +1,26 @@
 import products from '/products.js';
 import cart from './cart.js';
 
+
+document.addEventListener('DOMContentLoaded', () => {
 let listProduct = document.getElementById('listProduct');
 let app = document.getElementById('app');
 let temporaryContent = document.getElementById('temporaryContent');
 
 const loadTemplate = () => {
-    fetch('/template.html')
+    fetch('./template.html')
     .then(response => response.text())
     .then(html => {
         app.innerHTML = html;
         let contentTab = document.getElementById('contentTab');
         contentTab.innerHTML = temporaryContent.innerHTML;
-        temporaryContent.innerHTML = null;
+        temporaryContent.innerHTML = '';
         cart();
         initApp();
     })
 }
 loadTemplate();
+});
 const initApp = () => {
     let productId = new URLSearchParams(window.location.search).get('id');
     let thisProduct = products.filter(value => value.id == productId)[0];
